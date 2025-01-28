ab4252124a540bfaa1d95e2c3716c798f3903293
Tharushika <maxtharuz4@gmail.com>
2025-01-28 18:26:14 +0530

    code is updated

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full m-auto">
+      <div className="container bg-slate-100">
+        <h1 className="text-red-600">Hello world</h1>
+      </div>
+      
+    </div>
+  );
+}
